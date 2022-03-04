15eaf98739b118759e92fc331f7080d87219609a
Vasu Mistry <vasu5235@gmail.com>
2022-03-04 00:45:19 -0500

    category MVC

---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace marketplace_services_CSI5112.Models
+{
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string ImageURL { get; set; }
+        public string Name { get; set; }
+
+        public Category(int Id, string Name, string ImageURL)
+        {
+            this.Id = Id;
+            this.ImageURL = ImageURL;
+            this.Name = Name;
+        }
+    }
+}
+
