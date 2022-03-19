742c6fbf04b94708ae41ac1517b89fc3fce306a5
Vasu Mistry <vasu5235@gmail.com>
2022-03-19 15:15:29 -0400

    Orderservice validations

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -24,38 +24,51 @@ public class OrderController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<Dictionary<String, List<Product>>>> Get()
     {
+        Dictionary<String, List<Product>> allOrders = await _orderService.GetOrders();
+
+        if (allOrders.Keys.Count == 0)
+            return NotFound("No orders in database");
         return await _orderService.GetOrders();
     }
 
     [HttpGet("byUser/{UserId}")]
     public async Task<ActionResult<Dictionary<String, List<Product>>>> GetOrdersByUser(String UserId)
     {
+        if (UserId == null)
+            return NotFound("Userid cannot be null");
+
         Dictionary<String, List<Product>> FilteredOrders = await _orderService.GetOrdersByUserId(UserId);
 
-        //maybe used here
-        //if (FilteredOrders.Count == 0)
-        //    return NotFound();
+        if (FilteredOrders.Keys.Count == 0)
+            return NotFound("No Products found for this user");
+
         return FilteredOrders;
     }
 
     [HttpGet("byOrder/{OrderId}")]
     public async Task<ActionResult<List<Product>>> GetOrder(String OrderId)
     {
+        if (OrderId == null)
+            return NotFound("Orderid cannot be null");
         Console.WriteLine("--- debug ---- order.Id: " + OrderId);
 
-        //maybe used here
-        //bool orderExists = await _orderService.OrderExists(id);
-
-        //if (!orderExists)
-        //    return NotFound();
-
         List<Product> order = await _orderService.GetOrdersByOrderId(OrderId);
+
+        if (order.Count == 0)
+            return NotFound("No products found for this orderid");
+
         return order;
     }
 
     [HttpPost]
     public async Task<ActionResult<bool>> AddOrder(String id, List<Product> products)
     {
+        if (id == null || products.Count == 0)
+            return NotFound("Either id is null or productList is empty");
+
+        if (id.Split('-').Length != 2)
+            return NotFound("OrderId path param should be in format 'userId-orderId' example 123-456");
+
         bool result = await _orderService.AddOrder(id, products);
 
         return result;
